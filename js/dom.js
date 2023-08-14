9b302a84bb078d9d4cc23c2af6e287824b97a304
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-14 22:27:36 +0600

    Added DOM

--- js/dom.js
+++ js/dom.js
@@ -1,2 +1,16 @@
 console.log("Hello from TRISHA");
-console.log(document.body)
+console.log(document.body);
+
+const liCollection=document.getElementsByTagName('li');
+// console.log(liCollection);
+
+// for (const li of liCollection){
+//     console.log(li.innerText)
+// }
+
+const className=document.getElementsByClassName('list-name');
+console.log(className)
+
+const idName=document.getElementById('bash')
+idName.innerText="They never changed at all!";
+console.log(idName)
