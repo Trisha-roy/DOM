9819bd6fdc58ba93d9a0ff5ff6c90eeab99ed770
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-15 01:27:52 +0600

    Added appenChild

---
+++ js/append.js
@@ -0,0 +1,9 @@
+//where to add
+const list=document.getElementById('some-random-name');
+
+//what to be added
+const li=document.createElement('li');
+li.innerText="Some random name";
+
+//add the child
+list.appendChild(li)
