6a7c7a2a8d9a3d4583ddd8008ac0fa316237e6cd
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-16 00:22:36 +0600

    Added ButtonOnclick Property

--- js/append.js
+++ js/append.js
@@ -7,3 +7,34 @@ li.innerText="Some random name";
 
 //add the child
 list.appendChild(li)
+
+
+const mainConatainer=document.getElementById('main-container');
+console.log(mainConatainer);
+
+const section=document.createElement('section');
+const h1=document.createElement('h1');
+h1.innerText='My Food List'
+section.appendChild(h1)
+
+const ul=document.createElement('ul');
+const li1=document.createElement('li');
+const li2=document.createElement('li');
+const li3=document.createElement('li');
+const li4=document.createElement('li');
+
+li1.innerText='biriyani';
+ul.appendChild(li1);
+
+li2.innerText='biriyani';
+ul.appendChild(li2);
+
+li3.innerText='biriyani';
+ul.appendChild(li3);
+
+li4.innerText='biriyani';
+ul.appendChild(li4);
+
+section.appendChild(ul)
+
+mainConatainer.appendChild(section)
