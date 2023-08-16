6b34c468882e1072c91346e24e17516f7e854357
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-16 23:35:48 +0600

    Display Comment

--- js/comment-box.js
+++ js/comment-box.js
@@ -1,5 +1,18 @@
-document.getElementById('btn-post').addEventListener('click',function (){
-   const newComment= document.getElementById('new-comment');
-   const commentText=newComment.value;
-   console.log(commentText)
+// document.getElementById('btn-post').addEventListener('click',function (){
+//    const newComment= document.getElementById('new-comment');
+//    const commentText=newComment.value;
+//    console.log(commentText)
+// })
+
+document.getElementById('btn-post').addEventListener('click',function(){
+    const commentBox=document.getElementById('new-comment');
+    const textBox=commentBox.value;
+    //append
+    const commentContainer=document.getElementById('comment-container');
+    const p=document.createElement('p');
+    p.innerText=textBox;
+    commentContainer.appendChild(p)
+
+    //clear the box
+    commentBox.value='';
 })
