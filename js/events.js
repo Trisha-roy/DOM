596e5b0ae24c3a76f7990a064ef9a8404bb4c243
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-16 16:42:27 +0600

    Add some event handlers property

--- js/events.js
+++ js/events.js
@@ -7,3 +7,24 @@ function onclickStatus(){
     const handleStatus=document.getElementById('handler-status');
     handleStatus.innerText='This is not default Text'
 }
+
+function anotherButton(){
+    document.getElementById('handler-status').innerText='This is another default Text';
+}
+//Event Listener Button
+
+document.getElementById('event-button').addEventListener('click',function event(){
+    const eventHandler=document.getElementById('handler-status');
+    eventHandler.innerText='This is event listener Button'
+})
+
+//input field
+document.getElementById('update').addEventListener('click',function update(){
+    const inputField=document.getElementById('input-value');
+    const inputText=inputField.value;
+
+    const p=document.getElementById('input-text');
+    p.innerText=inputText;
+
+    inputField.value='';
+})
