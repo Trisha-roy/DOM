bd4b1e77b0bc531ba23bd5b926f493c796bbf54f
Trisha-roy <moumitaroytrisha72409@gmail.com>
2023-08-16 15:10:50 +0600

    Add EVent listener

--- js/events.js
+++ js/events.js
@@ -1,4 +1,9 @@
 document.getElementById('make-purple').addEventListener('click',
 function makePurple(){
     document.body.style.backgroundColor='purple'
-})
+});
+
+function onclickStatus(){
+    const handleStatus=document.getElementById('handler-status');
+    handleStatus.innerText='This is not default Text'
+}
